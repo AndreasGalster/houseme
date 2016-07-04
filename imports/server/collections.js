33d224acaf796844cb6b36cd68700a816984f7c6
Andreas Galster <andreas.galster@salarium.com>
2016-07-04 20:55:18 +0800

    removed unnecessary publications, structured and commented code

--- imports/server/collections.js
+++ imports/server/collections.js
@@ -22,6 +22,20 @@ Accounts.onCreateUser(function(options, user) {
 });
 
 
+
+
+
+
+
+/* User details
+ *
+ * Contains three subscriptions
+ * Subscribe to own user, only facebook fields
+ * Subscribe to all users, no limits
+ * Subscribe to latest logged in users via limiter
+ */
+
+
 // returns the own user with facebook fields
 Meteor.publish('user', function() {
   if (this.userId) {
@@ -29,7 +43,7 @@ Meteor.publish('user', function() {
       _id: this.userId
     }, {
       fields: {
-        'services.facebook': 1,
+       'services.facebook': 1,
       }
     });
   } else {
@@ -38,6 +52,7 @@ Meteor.publish('user', function() {
 });
 
 
+// Subscribe to all users, no limits
 Meteor.publish('usersTeaserAll', function () {
   return Meteor.users.find(
     {
@@ -45,7 +60,6 @@ Meteor.publish('usersTeaserAll', function () {
     },
     {
       fields: {
-        'profile': 1,
         'services.facebook.first_name': 1,
         'services.facebook.id': 1
       }
@@ -54,62 +68,7 @@ Meteor.publish('usersTeaserAll', function () {
 });
 
 
-
-
-
-
-
-
-Meteor.publish('userTeaser', function (otherUserId) {
-  return Meteor.users.findOne(
-    {
-      '_id': otherUserId
-    },
-    {
-      fields: {
-        'status': 1,
-        'profile': 1,
-        'services.facebook.first_name': 1,
-        'services.facebook.id': 1
-      }
-    }
-  );
-
-});
-
-
-Meteor.publish('PublicMessages', function (limiter) {
-  return PublicMessages.find(
-    {},
-    {
-      sort: {dateCreated: -1},
-      limit: limiter
-    }
-  );
-});
-
-
-Meteor.publish('SingleUserTeaser', function (toUserId) {
-  return Meteor.users.findOne(
-    {
-      '_id': toUserId
-    },
-    {
-      fields: {
-        'services.facebook.first_name': 1,
-        'services.facebook.id': 1
-      }
-    }
-  );
-});
-
-
-
-
-
-
-
-
+// Subscribe to latest logged in users via limiter
 Meteor.publish('usersTeaser', function (limiter) {
   return Meteor.users.find(
     {
@@ -129,8 +88,29 @@ Meteor.publish('usersTeaser', function (limiter) {
       limit: limiter
     }
   );
-
 });
+
+
+
+
+
+/* Public Messaging
+ *
+ * Contains one subscription
+ * Subscribe to all messages based on limiter,
+ * sorts newest messages first
+ */
+Meteor.publish('PublicMessages', function (limiter) {
+  return PublicMessages.find(
+    {},
+    {
+      sort: {dateCreated: -1},
+      limit: limiter
+    }
+  );
+});
+
+
 
 
 
@@ -148,7 +128,6 @@ Meteor.publish('PrivateMessagesUsers', function (limiter) {
   return Meteor.users.find(
     {
       'profile.active': true
-      // 'profile.active': true,
       // '_id': {$ne: this.userId}
 
     },
@@ -160,17 +139,13 @@ Meteor.publish('PrivateMessagesUsers', function (limiter) {
       // limit: limiter
     }
   );
-
 });
-
-
 
 
 // retrieves the list of messages sent/received
 Meteor.publish('PrivateMessagesList', function () {
   return PrivateMessagesList.find({ $or: [{toUserId: this.userId}, {fromUserId: this.userId}] });
 });
-
 
 
 // retrieves content of one individual message stream
